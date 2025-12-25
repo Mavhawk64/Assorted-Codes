92685671787c8174f5bd89f6d02f974dc2bedc5e
Maverick <mavberk@gmail.com>
2025-12-24 19:29:31 -0500

    Added picross -- probably removing / adding to actual repo lol

---
+++ picross-stuff/list_checker.py
@@ -0,0 +1,150 @@
+def remove_incorrect_guesses_by_row_col(guesses, row_col):
+    # removes guesses that contradict known row/col info
+    filtered_guesses = []
+    for guess in guesses:
+        valid = True
+        for i in range(len(guess)):
+            if row_col[i] is not None and guess[i] != row_col[i]:
+                valid = False
+                break
+        if valid:
+            filtered_guesses.append(guess)
+    return filtered_guesses
+
+
+def form_blocks(arr):
+    # converts [1,1,0,1,0] to [2,0,1,0]
+    blocks = []
+    for i in arr:
+        if i == 1:
+            if len(blocks) == 0 or blocks[-1] == 0:
+                blocks.append(1)
+            else:
+                blocks[-1] += 1
+        else:
+            blocks.append(0)
+    return blocks
+
+
+def are_valid_blocks(guess_blocks, target_blocks):
+    # if target_blocks[0] in guess_blocks, find index (idx) and check rest...
+    # if target_blocks[1] in guess_blocks[idx+1:], find index (idx) and check rest...
+    for target in target_blocks:
+        found = False
+        for idx, guess in enumerate(guess_blocks):
+            if guess == target:
+                found = True
+                guess_blocks = guess_blocks[idx + 1 :]
+                break
+        if not found:
+            return False
+    return True
+
+
+def get_valid_blocks(guesses, target_blocks):
+    valid_guesses = []
+    for guess in guesses:
+        if are_valid_blocks(guess, target_blocks):
+            valid_guesses.append(guess)
+    return valid_guesses
+
+
+def expand_blocks(blocks):
+    # expands [2,0,1,0] to [1,1,0,1,0]
+    expanded = []
+    for block in blocks:
+        if block == 0:
+            expanded.append(0)
+        else:
+            expanded.extend([1] * block)
+    return expanded
+
+
+def intersection(lists):
+    intersection = lists[0]
+    for li in range(1, len(lists)):
+        intersection = [
+            a if a == b else None for a, b in zip(intersection, lists[li], strict=False)
+        ]
+    return intersection
+
+
+def fill_nones_with_zeros(row_col):
+    return [0 if cell is None else cell for cell in row_col]
+
+
+def quick_remove_junk(guesses, total_filled):
+    filtered = []
+    for guess in guesses:
+        if sum(guess) == total_filled:
+            filtered.append(guess)
+    return filtered
+
+
+def run_checker(LIST, ROW_COL, debug=False):
+    LENGTH = len(ROW_COL)
+    if sum(LIST) == ROW_COL.count(1):
+        if debug:
+            print("Already solved!")
+        return fill_nones_with_zeros(ROW_COL)
+    guesses = []
+    # guesses (for length 5 like above) = [[0,0,0,0,0], [0,0,0,0,1], [0,0,0,1,0], ..., [1,1,1,1,1]]
+    # then we verify later
+    for i in range(2**LENGTH):
+        guess = []
+        for j in range(LENGTH):
+            if (i >> j) & 1:
+                guess.append(1)
+            else:
+                guess.append(0)
+        guesses.append(guess)
+    if debug:
+        print("Total guesses before quick removal:", len(guesses))
+    guesses = quick_remove_junk(guesses, sum(LIST))
+    if debug:
+        print("Total guesses after quick removal:", len(guesses))
+
+    guesses = remove_incorrect_guesses_by_row_col(guesses, ROW_COL)
+    if debug:
+        print("Total guesses after removing contradictions:", len(guesses))
+
+    guesses = list(set(tuple(guess) for guess in guesses))  # remove duplicates
+
+    guesses = [list(guess) for guess in guesses]
+    if debug:
+        print(len(guesses))
+
+    guesses = [form_blocks(guess) for guess in guesses]
+    # if debug:
+    #     print(guesses)
+    valid_guesses = get_valid_blocks(guesses, LIST)
+    if debug:
+        print(valid_guesses)
+    expanded_valid_guesses = [expand_blocks(guess) for guess in valid_guesses]
+    # if debug:
+    #     print(expanded_valid_guesses)
+
+    final_intersection = intersection(expanded_valid_guesses)
+    # if debug:
+    #     print(final_intersection)
+
+    return final_intersection
+
+
+# Fill it in like binary numbers:
+# 0000 -> 0001, 0010, 0011, 0100, ..., 1111
+# by this, i mean (in a fairly simple case):
+# LIST = [2, 1]
+# ROW_COL = [None, None, None, None, None]
+# Fill in the Nones -> [1,1,0,1,0], [1,1,0,0,1], [0,1,1,0,1] are the only valid options
+# Technically, I could just fill this in with the combinations or whatever then do a check to make sure that blocks are in the right place
+# Let's try that:
+
+if __name__ == "__main__":
+    # LIST = [2, 4, 1, 2]  # list of blocks
+    # ROW_COL = [0, 0, 0, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 0, 0]  # fmt: skip
+    LIST = [12]
+    ROW_COL = [None, None, None, None, None, None, 1,1,1,1,1,1,None, None, 1, None, None, None, 0, 0]  # fmt: skip
+    # None - unknown, 0 - empty, 1 - filled.
+    result = run_checker(LIST, ROW_COL, debug=True)
+    print("Result:", result)
---
+++ picross-stuff/main.py
@@ -0,0 +1,213 @@
+import ast
+import os
+import re
+
+import numpy as np
+from list_checker import run_checker
+
+HAS_PARTIAL_HINTS = False
+
+INPUT = "wario_9b.txt"
+
+
+def display_grid(
+    grid, row_col_idx: tuple | None = None, output_file: str | None = None
+):
+    # [♦] for filled, [X] for empty, [_] for unknown
+    # highlight (blue) every 5 columns/rows (except edges)
+    total_output = ""
+    for col in np.array(COL_NUMS_DISPLAY).T:
+        out = " " * 8 * 3  # space for row numbers
+        for _col_idx, num in enumerate(col):
+            if _col_idx != 0 and _col_idx % 5 == 0:
+                out += " "
+            if num is None:
+                out += "   "
+            else:
+                out += " " * (2 - len(str(num))) + f"{num} "
+        if not output_file:
+            print(out)
+        total_output += out + "\n"
+    for _row_idx, row in enumerate(grid):
+        display_row = ""
+        row_fmt = ""
+        row_end_fmt = ""
+        if _row_idx != 0 and _row_idx % 5 == 0:
+            display_row += (" " * 8 * 3) + (
+                "\033[94m" + "-" * (WIDTH * 3 + (WIDTH // 5) - 1) + "\033[0m\n"
+            )
+
+        for idx in range(len(ROW_NUMS_DISPLAY[_row_idx])):
+            num = ROW_NUMS_DISPLAY[_row_idx][idx]
+            if num is None:
+                display_row += "   "
+            else:
+                display_row += " " * (2 - len(str(num))) + f"{num} "
+        if row_col_idx is not None and row_col_idx[0] == _row_idx:
+            row_fmt = "\033[93m"
+            row_end_fmt = "\033[0m"
+        display_row += row_fmt
+        for _col_idx, cell in enumerate(row):
+            col_fmt = ""
+            col_end_fmt = ""
+
+            if row_col_idx is not None and row_col_idx[1] == _col_idx:
+                col_fmt = "\033[93m"
+                col_end_fmt = "\033[0m"
+
+            if _col_idx != 0 and _col_idx % 5 == 0:
+                display_row += row_end_fmt + "\033[94m" + "|" + "\033[0m" + row_fmt
+            if cell == 1:
+                display_row += row_end_fmt + "\033[92m" + "[♦]" + "\033[0m" + row_fmt
+            elif cell == 0:
+                display_row += col_fmt + "[X]" + col_end_fmt
+            else:
+                display_row += col_fmt + "[_]" + col_end_fmt
+        if row_col_idx is not None and row_col_idx[0] == _row_idx:
+            display_row += row_end_fmt
+        if not output_file:
+            print(display_row)
+        total_output += display_row + "\n"
+    if output_file is not None:
+        with open(output_file, "w", encoding="utf-8") as f:
+            # remove ANSI codes for the file output
+            ansi_escape = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")
+            total_output = ansi_escape.sub("", total_output)
+            f.write(total_output)
+
+
+if not HAS_PARTIAL_HINTS:
+    # Ship (Mario 1-J)
+    # COLUMNS = [[1], [2], [1, 1, 3], [5, 3], [5, 3], [10], [5, 3], [5, 3], [1, 1, 3], [2]]
+    # ROWS = [[1], [7], [5], [5], [5], [7], [1], [10], [9], [7]]
+
+    # (Wario 9-A)
+    with open(os.path.join(os.path.dirname(__file__), "puzzles", INPUT), "r") as f:
+        content = f.read().strip().split("\n")
+        COLUMNS = [[int(x) for x in a.split(",")] for a in content[0].split(" ")]
+        ROWS = [[int(x) for x in a.split(",")] for a in content[1].split(" ")]
+
+    print("COLUMNS:", COLUMNS)
+    print("ROWS:", ROWS)
+
+    WIDTH = len(COLUMNS)
+    HEIGHT = len(ROWS)
+
+    # Fill in with Nones up to 8
+    COL_NUMS_DISPLAY = [[None] * (8 - len(lst)) + lst for lst in COLUMNS]
+    ROW_NUMS_DISPLAY = [[None] * (8 - len(lst)) + lst for lst in ROWS]
+
+    GRID = [[None for _ in range(WIDTH)] for _ in range(HEIGHT)]
+
+    print("Parsed input as:")
+    display_grid(GRID)
+    input("Press Enter to start solving (or Ctrl+C to cancel)...")
+else:
+    # open partial hints file
+    with open(
+        os.path.join(os.path.dirname(__file__), "partial_solution.txt"),
+        "r",
+        encoding="utf-8",
+    ) as f:
+        content = f.read().split("\n")
+    # first 8 lines are column hints
+    COL_NUMS_DISPLAY = []
+    for i in range(8):
+        line = content[i][8 * 3 :]
+        COL_NUMS_DISPLAY.append([])
+        counter = 1
+        while True:
+            next_num = line[: 3 + (1 if counter % 5 == 0 else 0)]
+            if next_num.strip() == "":
+                COL_NUMS_DISPLAY[-1].append(None)
+            else:
+                COL_NUMS_DISPLAY[-1].append(int(next_num.strip()))
+            line = line[3 + (1 if counter % 5 == 0 else 0) :]
+            counter += 1
+            if line == "":
+                break
+    COL_NUMS_DISPLAY = np.array(COL_NUMS_DISPLAY).T.tolist()
+    # COLUMNS = remove nones from COL_NUMS_DISPLAY
+    COLUMNS = [[num for num in col if num is not None] for col in COL_NUMS_DISPLAY]
+
+    # Parse ROW_NUMS_DISPLAY and ROWS
+    ROW_NUMS_DISPLAY = []
+    ROWS = []
+    counter = 1
+    for i in range(8, len(content)):
+        if counter % 6 == 0 and counter != 0:
+            counter += 1
+            continue
+        counter += 1
+        line = content[i][: 8 * 3]
+        ROW_NUMS_DISPLAY.append([])
+        for _ in range(0, 8):
+            next_num = line[:3]
+            if next_num.strip() == "":
+                ROW_NUMS_DISPLAY[-1].append(None)
+            else:
+                num = int(next_num.strip())
+                ROW_NUMS_DISPLAY[-1].append(num)
+            line = line[3:]
+    print(ROW_NUMS_DISPLAY)
+    # ROWS = remove nones from ROW_NUMS_DISPLAY
+    ROWS = [[num for num in row if num is not None] for row in ROW_NUMS_DISPLAY]
+
+    # parse grid
+    GRID = []
+    grid_txt = "["
+    for i in range(8, len(content)):
+        if (i - 8 + 1) % 6 == 0 and (i - 8 + 1) != 0:
+            continue
+        line = content[i][8 * 3 :].replace("|", "").replace("][", ",").replace(
+            "♦", "1"
+        ).replace("X", "0").replace("_", "None") + (",")
+        grid_txt += line
+    grid_txt = grid_txt[:-1] + "]"
+    GRID = ast.literal_eval(grid_txt)
+
+    WIDTH = len(COLUMNS)
+    HEIGHT = len(ROWS)
+    print("PARSED GRID:")
+    display_grid(GRID)
+
+
+prev_grid = None
+cnt = 1
+while prev_grid != GRID:
+    prev_grid = [row[:] for row in GRID]
+    print("Processing Rows:")
+    for row_idx in range(HEIGHT):
+        row_result = run_checker(ROWS[row_idx], GRID[row_idx], debug=False)
+        GRID[row_idx] = row_result
+        display_grid(GRID, row_col_idx=(row_idx, None))
+        print(f"^^^^ Row {row_idx + 1}/{HEIGHT} ^^^^\n")
+        # time.sleep(1)
+    print("Processing Columns:")
+    for col_idx in range(WIDTH):
+        col = [GRID[row_idx][col_idx] for row_idx in range(HEIGHT)]
+        col_result = run_checker(COLUMNS[col_idx], col, debug=False)
+        for row_idx in range(HEIGHT):
+            GRID[row_idx][col_idx] = col_result[row_idx]
+        display_grid(GRID, row_col_idx=(None, col_idx))
+        print(f"^^^^ Column {col_idx + 1}/{WIDTH} ^^^^\n")
+        # time.sleep(1)
+    # exit()
+    print(f"After iteration #{cnt}:")
+    display_grid(GRID)
+    cnt += 1
+    if None not in [cell for row in GRID for cell in row]:
+        print("Solved!")
+        break
+
+print("Final Grid:")
+display_grid(GRID)
+
+if None in [cell for row in GRID for cell in row]:
+    print(
+        "Could not solve the puzzle completely. Saving current state to 'partial_solution.txt'. Feel free to update it with any hints you have and rerun the solver."
+    )
+    display_grid(
+        GRID,
+        output_file=os.path.join(os.path.dirname(__file__), "partial_solution.txt"),
+    )
